6ed53281334dbeeef62f83e38acfb0cf6d6f34a4
Lapis0875 <lapis0875@gmail.com>
2020-04-16 09:49:16 +0900

    Initial commit

---
+++ src/main/java/kr/ojr/lapis/citizenquest/CitizenQuest.java
@@ -0,0 +1,21 @@
+package kr.ojr.lapis.citizenquest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+// import for colored console
+import kr.ojr.lapis.citizenquest.console.FormatedConsole;
+import kr.ojr.lapis.citizenquest.console.LogType;
+
+public final class CitizenQuest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/kr/ojr/lapis/citizenquest/console/LogType.java
@@ -0,0 +1,8 @@
+package kr.ojr.lapis.citizenquest.console;
+
+public enum LogType{
+    LOG,
+    INFO,
+    DEBUG,
+    ERROR
+}
