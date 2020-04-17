06c24d2cb24fc04d86d5707b9d3c054da735b2d0
Lapis0875 <lapis0875@gmail.com>
2020-04-17 22:14:32 +0900

    Create .gitignore

--- src/main/java/kr/ojr/lapis/citizenquest/CitizenQuest.java
+++ src/main/java/kr/ojr/lapis/citizenquest/CitizenQuest.java
@@ -8,14 +8,17 @@ import kr.ojr.lapis.citizenquest.console.LogType;
 
 public final class CitizenQuest extends JavaPlugin {
 
+    FormatedConsole formatedConsole = new FormatedConsole(this.getDescription());
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        formatedConsole.console(LogType.INFO, "Plugin Enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        formatedConsole.console(LogType.INFO, "Plugin Disabled!");
     }
 }
